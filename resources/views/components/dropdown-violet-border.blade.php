0704d00186b51cd785202fa27b98f9a65a6be793
ragnaros1337 <torus.gothic@gmail.com>
2022-08-26 12:24:33 +0300

    11

--- resources/views/components/dropdown-violet-border.blade.php
+++ resources/views/components/dropdown-violet-border.blade.php
@@ -2,7 +2,7 @@
 <div class="dropdown-violet-border">
     <button class="dropdown-violet-border-btn small-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
         {{ $slot }}
-        <mat-icon fontSet="material-symbols-rounded">face</mat-icon>
+{{--        <mat-icon fontSet="material-symbols-rounded">face</mat-icon>--}}
     </button>
     <ul class="dropdown-menu dropdown-violet-border-menu">
         @foreach($list as $key => $value)
