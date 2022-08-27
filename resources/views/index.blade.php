c4efbacc3025b8699d1927a8e9d2368cf8cdeac4
ragnaros1337 <torus.gothic@gmail.com>
2022-08-27 15:17:54 +0300

    2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,13 @@
         <x-btn :classes="'grey small-btn'">Для города</x-btn>
         <x-btn :classes="'orange big-btn'">Оформить предзаказ</x-btn>
         @php $items = [1 => 'Сначала дорогие',2 => 'Сначала дешевые'] @endphp
-        <x-dropdown :list="$items" :classes="'violet-border'">По цене</x-dropdown>
+        <x-dropdown :list="$items" :classes="'violet-border'">По цене <span class="material-symbols-outlined">circle</span></x-dropdown>
         <x-btn :classes="'white big-btn'">Перейти в каталог</x-btn>
         @php $items = [1 => 'Самокаты',2 => 'Аксессуары'] @endphp
         <x-dropdown :list="$items" :classes="'grey'">Везде</x-dropdown>
         <x-btn :classes="'violet big-btn btn-catalog'">Каталог <div class="btn-catalog-content"><i class="btn-catalog-icon"></i></div></x-btn>
         <x-link :href="''" :classes="'black'">Сервис</x-link>
+        <x-link :href="''" :classes="'violet'">О магазине</x-link>
+        <x-socials :height="16"></x-socials>
     </div>
 @endsection
